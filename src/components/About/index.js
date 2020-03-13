6267d451c794a58a5b3a1f69e20b2da5944b1e78
Shani Almog <killerdrum@gmail.com>
2020-03-13 22:55:41 +0200

    first commit

---
+++ src/components/About/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Blah blah blah about</p>
+        </div>
+    )
+}
+
+
+export default About
---
+++ src/components/Contact/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Contact() {
+    return (
+        <div>
+            <h1>Contact</h1>
+            <p>Contact us through candy</p>
+        </div>
+    )
+}
+
+
+export default Contact
